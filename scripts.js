6a555f0349cde78faf937deb26f57a03ef17c214
MathisChatelain <mathis.chatelain2001@gmail.com>
2022-02-07 22:25:35 +0100

    simple movements

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
 class Player{
-    size = 100;            // Soit un rayon de size/2 
-    speed = 100;           // Valeur en pixels par seconde
-    id = "00000000"        // Un identifiant à 8 chiffres doit être déclaré à la création d'un joueur
-    posX = 0;
-    posY = 0;
+    // Gestion
+    id = "00000000" // Un identifiant à 8 chiffres doit être déclaré à la création d'un joueur
+    
+    // Graphisme
+    sizeX = 100;
+    sizeY = 100;     
+    
+    // Physique
+    vitesseActuelle = 0;       // pixels par update
+    vitesseDeBase = 3;
+    isMoving = false;
+    posX = 700; // Centre le joueur au départ
+    posY = 300;
+    targetX = 0;
+    targetY = 0;
+    cosTarget = 0;          // angles de la vitesse par rapport à target ( plus pratique que l'orientation en degrés )
+    sinTarget = 0;
+    
+    // DOM 
     playerElement = document.createElement('div');
 
     constructor(id) {
@@ -12,28 +26,85 @@ class Player{
         document.getElementById("playerList").append(this.playerElement);
     }
 
-    // addMovementsListener() {
-    // document.addEventListener('mousemove', e => {
-    //     let x = e.clientX -this.size/2;
-    //     let y = e.clientY -this.size/2;;
-    //     this.playerElement.style.top = y.toString()+"px";
-    //     this.playerElement.style.left = x.toString()+"px";
-    // });
-    // }
+    setMove(e){
+        this.targetX = e.clientX;         // change la position que le player doit rejoindre
+        this.targetY = e.clientY;          
+        // note l'orientation relative à target
+        let adjacent = this.targetX-this.posX;
+        let oppose = this.targetY-this.posY;
+        let hypotenuse = Math.hypot(oppose,adjacent);
 
+        this.sinTarget = oppose/hypotenuse; 
+        this.cosTarget = adjacent/hypotenuse;
+        this.isMoving = true;
+    }
 
-    addMovementsListener() {                            // Clique droit
-    document.addEventListener('contextmenu', e => {
-        e.preventDefault();
-        let x = e.clientX -this.size/2;
-        let y = e.clientY -this.size/2;
-        this.posX = x;
-        this.posY = y;
-        this.playerElement.style.top = y.toString()+"px";
-        this.playerElement.style.left = x.toString()+"px";
-    });
+    stopPlayer(){
+        this.vitesseActuelle = 0;
+        this.targetX = this.posX;
+        this.targetY = this.posX;
+        this.isMoving = false;
+    }
+
+    fire(e){
+        this.stopPlayer();
+        
+        let X = e.clientX;         // change la position que le player doit rejoindre
+        let Y = e.clientY;          
+        let adjacent = X-this.posX;
+        let oppose = Y-this.posY;
+        let hypotenuse = Math.hypot(oppose,adjacent);
+
+        let sinCible = oppose/hypotenuse; 
+        let cosCible = adjacent/hypotenuse;
+        
+        let newProjectile = new Projectiles(this,"TypeDuProjectile",cosCible,sinCible);
+        newProjectile.activate();
+    }
+
+    addMovementsListeners() {                            
+        document.addEventListener('contextmenu', e => {         // Clique droit
+            e.preventDefault();                                 // Empeche l'ouverture du menu déroulant du clique droit
+        });
+
+        document.addEventListener('mousedown', e => {      
+            switch (e.button){ 
+                case 0 :            // Clique gauche
+                    this.fire(e);
+                    break
+                
+                case 2 :             // Clique droit
+                    this.setMove(e);
+                    break
+            }
+        });
+    }
+
+    updateMovements() {
+        let distanceToTarget = Math.hypot((this.targetX-this.posX),(this.targetY-this.posY));
+        if( distanceToTarget > 5 && this.isMoving == true){ // Si on est pas très proche de la cible
+            this.vitesseActuelle = this.vitesseDeBase;
+            this.posX += this.vitesseActuelle*this.cosTarget; // Vitesse horizontale
+            this.posY += this.vitesseActuelle*this.sinTarget; // Vitesse verticale
+        }
+        else{
+            this.vitesseActuelle = 0;
+            this.isMoving = false;
+        }
+    }
+
+    updateRendering() {
+        this.playerElement.style.top = this.posY.toString()+"px";
+        this.playerElement.style.left = this.posX.toString()+"px";
     }
 }
 
 let player = new Player("00000000");
-player.addMovementsListener();
+player.addMovementsListeners();
+
+var frameUpdater = window.setInterval(function(){
+    player.updateMovements();
+    player.updateRendering();
+  }, 8); // 60 FPS
+
+// clearInterval(frameUpdater) 
